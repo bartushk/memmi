adc6a5452ea71047cebe6cefd06e98b796841ad5
Kyle Bartush <bartushk@gmail.com>
2017-01-20 17:15:48 -0800

    Put the memmi logo in the AppBar

--- client-side/src/components/Header/Header.js
+++ client-side/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
-import logoUrl from './logo.png'
+import logoUrl from './logo_67x50.svg'
 import './Header.scss'
 
 
@@ -8,7 +8,7 @@ class Header extends React.Component {
   render() {
     return (
       <AppBar title="Memmi" className="header">
-        <img src={logoUrl} className="logo" />
+        <img src={logoUrl} />
       </AppBar>
     )
   }
