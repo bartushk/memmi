18d19950c1662a5201e1dea7b0bc004fdc6a003d
Kyle Bartush <bartushk@gmail.com>
2017-02-12 23:20:36 -0800

    Update to test script. Added rest of in memory card management tests.

--- web/src/memmi/card/inmemory-card-management.go
+++ web/src/memmi/card/inmemory-card-management.go
@@ -3,11 +3,18 @@ package card
 import (
 	"errors"
 	"fmt"
+	"hash/fnv"
 	"memmi/pbuf"
 )
 
 func (manager *InMemoryCardManagement) getKey(id []byte) string {
 	return fmt.Sprintf("%x", id)
+}
+
+func (manager *InMemoryCardManagement) getId(input uint32) []byte {
+	hash := fnv.New64()
+	hash.Write([]byte(fmt.Sprintf("%x", input)))
+	return hash.Sum(nil)
 }
 
 func NewInMemoryManagement() *InMemoryCardManagement {
@@ -19,8 +26,10 @@ func NewInMemoryManagement() *InMemoryCardManagement {
 }
 
 type InMemoryCardManagement struct {
-	cardSets map[string]pbuf.CardSet
-	cards    map[string]pbuf.Card
+	cardSets       map[string]pbuf.CardSet
+	cards          map[string]pbuf.Card
+	cardCounter    uint32
+	cardSetCounter uint32
 }
 
 func (manager *InMemoryCardManagement) GetCardSetById(id []byte) pbuf.CardSet {
@@ -39,6 +48,7 @@ func (manager *InMemoryCardManagement) DeleteCardSetById(id []byte) error {
 	if !ok {
 		return errors.New("CardSet with that ID does not exist and could not be deleted.")
 	}
+	delete(manager.cardSets, key)
 	return nil
 }
 
@@ -48,13 +58,38 @@ func (manager *InMemoryCardManagement) DeleteCardById(id []byte) error {
 	if !ok {
 		return errors.New("Card with that ID does not exist and could not be deleted.")
 	}
+	delete(manager.cardSets, key)
 	return nil
 }
 
 func (manager *InMemoryCardManagement) SaveCardSet(set *pbuf.CardSet) ([]byte, error) {
-	return nil, nil
+	if set == nil {
+		return nil, errors.New("Cannot save nil.")
+	}
+	id := manager.getId(manager.cardSetCounter)
+	key := manager.getKey(id)
+	_, ok := manager.cardSets[key]
+	if ok {
+		return nil, errors.New("There was a key collision and your card set could not be saved.")
+	}
+	set.Id = id
+	manager.cardSets[key] = *set
+	manager.cardSetCounter += 1
+	return id, nil
 }
 
 func (manager *InMemoryCardManagement) SaveCard(card *pbuf.Card) ([]byte, error) {
-	return nil, nil
+	if card == nil {
+		return nil, errors.New("Cannot save nil.")
+	}
+	id := manager.getId(manager.cardCounter)
+	key := manager.getKey(id)
+	_, ok := manager.cards[key]
+	if ok {
+		return nil, errors.New("There was a key collision and your card could not be saved.")
+	}
+	card.Id = id
+	manager.cards[key] = *card
+	manager.cardCounter += 1
+	return id, nil
 }
--- web/src/memmi/card/inmemory-card-management_test.go
+++ web/src/memmi/card/inmemory-card-management_test.go
@@ -3,6 +3,7 @@ package card
 import (
 	"github.com/golang/protobuf/proto"
 	"memmi/pbuf"
+	"reflect"
 	"testing"
 )
 
@@ -75,4 +76,181 @@ func Test_InMemoryCardManagement_GetCardById_GoodKey_CardReturned(t *testing.T) 
 			"Expected:", testCard,
 			"Got:", result)
 	}
+}
+
+func Test_InMemoryCardManagement_DeleteCardSetById_BadKey_ErrorReturned(t *testing.T) {
+	newMan := NewInMemoryManagement()
+
+	result := newMan.DeleteCardSetById([]byte{1, 4, 6})
+
+	if result == nil {
+		t.Error("Error was not returned, it should have been.")
+	}
+}
+
+func Test_InMemoryCardManagement_DeleteCardSetById_GoodKey_CardSetRemoved(t *testing.T) {
+	newMan := NewInMemoryManagement()
+	testId := []byte{1, 2, 3}
+	testCardSet := pbuf.CardSet{
+		Id:      testId,
+		Version: 5,
+	}
+
+	blankSet := pbuf.CardSet{}
+	newMan.cardSets[newMan.getKey(testId)] = testCardSet
+
+	result := newMan.DeleteCardSetById(testId)
+	mapResult := newMan.cardSets[newMan.getKey(testId)]
+
+	if result != nil {
+		t.Error("No error should have been returned by delete operation.")
+	}
+
+	if !proto.Equal(&mapResult, &blankSet) {
+		t.Error("Wrong result returned",
+			"Expected:", blankSet,
+			"Got:", mapResult)
+	}
+}
+
+func Test_InMemoryCardManagement_SaveCardSet_PassedNil_ErrorReturned(t *testing.T) {
+	newMan := NewInMemoryManagement()
+
+	_, result := newMan.SaveCardSet(nil)
+
+	if result == nil {
+		t.Error("Error should be returned when passed nil.")
+	}
+}
+
+func Test_InMemoryCardManagement_SaveCardSet_DuplicateKey_ErrorReturned(t *testing.T) {
+	newMan := NewInMemoryManagement()
+	id := newMan.getId(newMan.cardSetCounter)
+	key := newMan.getKey(id)
+	newMan.cardSets[key] = pbuf.CardSet{}
+	_, result := newMan.SaveCardSet(&pbuf.CardSet{})
+
+	if result == nil {
+		t.Error("Error should be returned when passed nil.")
+	}
+}
+
+func Test_InMemoryCardManagement_SaveCardSet_MultipleSaves_SavedCorrectly(t *testing.T) {
+	newMan := NewInMemoryManagement()
+	id1 := newMan.getId(newMan.cardSetCounter)
+	key1 := newMan.getKey(id1)
+	id2 := newMan.getId(newMan.cardSetCounter + 1)
+	key2 := newMan.getKey(id2)
+	set1 := pbuf.CardSet{Version: 10}
+	set2 := pbuf.CardSet{Version: 30}
+
+	result1, err1 := newMan.SaveCardSet(&set1)
+	result2, err2 := newMan.SaveCardSet(&set2)
+
+	test1 := newMan.cardSets[key1]
+	test2 := newMan.cardSets[key2]
+
+	if err1 != nil {
+		t.Error("First save returned an error, should have returned nil:", err1)
+	}
+
+	if err2 != nil {
+		t.Error("Second save returned an error, should have returned nil:", err2)
+	}
+
+	if !reflect.DeepEqual(id1, result1) {
+		t.Error("Wrong id1 returned",
+			"Expected:", id1,
+			"Got:", result1)
+	}
+
+	if !reflect.DeepEqual(id2, result2) {
+		t.Error("Wrong id2 returned",
+			"Expected:", id2,
+			"Got:", result2)
+	}
+
+	if !proto.Equal(&test1, &set1) {
+		t.Error("Wrong card set saved to dictionary for first save.",
+			"Expected:", set1,
+			"Got:", test1)
+	}
+
+	if !proto.Equal(&test2, &set2) {
+		t.Error("Wrong card set saved to dictionary for second save.",
+			"Expected:", set2,
+			"Got:", test2)
+	}
+
+}
+
+func Test_InMemoryCardManagement_SaveCard_PassedNil_ErrorReturned(t *testing.T) {
+	newMan := NewInMemoryManagement()
+
+	_, result := newMan.SaveCard(nil)
+
+	if result == nil {
+		t.Error("Error should be returned when passed nil.")
+	}
+}
+
+func Test_InMemoryCardManagement_SaveCard_DuplicateKey_ErrorReturned(t *testing.T) {
+	newMan := NewInMemoryManagement()
+	id := newMan.getId(newMan.cardCounter)
+	key := newMan.getKey(id)
+	newMan.cards[key] = pbuf.Card{}
+	_, result := newMan.SaveCard(&pbuf.Card{})
+
+	if result == nil {
+		t.Error("Error should be returned when passed nil.")
+	}
+}
+
+func Test_InMemoryCardManagement_SaveCard_MultipleSaves_SavedCorrectly(t *testing.T) {
+	newMan := NewInMemoryManagement()
+	id1 := newMan.getId(newMan.cardCounter)
+	key1 := newMan.getKey(id1)
+	id2 := newMan.getId(newMan.cardCounter + 1)
+	key2 := newMan.getKey(id2)
+	card1 := pbuf.Card{Title: "Card One"}
+	card2 := pbuf.Card{Title: "Card Two"}
+
+	result1, err1 := newMan.SaveCard(&card1)
+	result2, err2 := newMan.SaveCard(&card2)
+
+	test1 := newMan.cards[key1]
+	test2 := newMan.cards[key2]
+
+	if err1 != nil {
+		t.Error("First save returned an error, should have returned nil:", err1)
+	}
+
+	if err2 != nil {
+		t.Error("Second save returned an error, should have returned nil:", err2)
+	}
+
+	if !reflect.DeepEqual(id1, result1) {
+		t.Error("Wrong id1 returned",
+			"Expected:", id1,
+			"Got:", result1)
+	}
+
+	if !reflect.DeepEqual(id2, result2) {
+		t.Error("Wrong id2 returned",
+			"Expected:", id2,
+			"Got:", result2)
+	}
+
+	if !proto.Equal(&test1, &card1) {
+		t.Error("Wrong card set saved to dictionary for first save.",
+			"Expected:", card1,
+			"Got:", test1)
+	}
+
+	if !proto.Equal(&test2, &card2) {
+		t.Error("Wrong card set saved to dictionary for second save.",
+			"Expected:", card2,
+			"Got:", test2)
+	}
+
 }
