2bafce6b3e231f6d205585df1e7579bbe6dc1b1f
Kyle Bartush <bartushk@gmail.com>
2017-02-14 23:03:33 -0800

    Mid work on user management. Going to bed.

---
+++ web/src/memmi/card/utils.go
@@ -0,0 +1,29 @@
+package card
+
+import (
+	"memmi/pbuf"
+)
+
+func GenerateEmptyHistory(cardSet *pbuf.CardSet) pbuf.UserHistory {
+	if cardSet == nil {
+		return pbuf.UserHistory{}
+	}
+	history := pbuf.UserHistory{
+		UserId:    []byte{},
+		CardSetId: cardSet.Id,
+		PlayIndex: 0,
+		IsEmpty:   len(cardSet.CardIds) > 0,
+		History:   []*pbuf.CardHistory{},
+	}
+	for i, cardId := range cardSet.CardIds {
+		cardHistory := &pbuf.CardHistory{
+			CardId:       cardId,
+			CurrentScore: 0,
+			CardIndex:    int32(i),
+			Scores:       []int32{},
+			Indicies:     []int32{},
+		}
+		history.History = append(history.History, cardHistory)
+	}
+	return history
+}
--- web/src/memmi/user/inmemory-user-management.go
+++ web/src/memmi/user/inmemory-user-management.go
@@ -1,29 +1,69 @@
 package user
 
 import (
+	"errors"
+	"fmt"
+	"hash/fnv"
+	"memmi/card"
 	"memmi/pbuf"
+	"reflect"
 )
 
 type InMemoryUserManagement struct {
 	userIds       map[string][]byte
 	authInfo      map[string]pbuf.UserAuthInfo
 	userHistories map[string]pbuf.UserHistory
+	userCounter   int64
+	cardMan       card.CardManagement
+}
+
+func (manager *InMemoryUserManagement) getKey(id []byte) string {
+	return fmt.Sprintf("%x", id)
+}
+
+func (manager *InMemoryUserManagement) getId(input uint32) []byte {
+	hash := fnv.New64()
+	hash.Write([]byte(fmt.Sprintf("%x", input)))
+	return hash.Sum(nil)
 }
 
 func (manager *InMemoryUserManagement) GetHistory(user pbuf.User, cardSetId []byte) pbuf.UserHistory {
-	return pbuf.UserHistory{}
+	fullId := append(user.Id, cardSetId...)
+	key := manager.getKey(fullId)
+	result, ok := manager.userHistories[key]
+	if !ok {
+		set := manager.cardMan.GetCardSetById(cardSetId)
+		result = card.GenerateEmptyHistory(&set)
+	}
+	return result
 }
 
 func (manager *InMemoryUserManagement) GetAuthInfoByUserName(userName string) pbuf.UserAuthInfo {
-	return pbuf.UserAuthInfo{}
+	userId := manager.userIds[userName]
+	return manager.GetAuthInfoById(userId)
 }
 
 func (manager *InMemoryUserManagement) UpdateHistory(user pbuf.User, cardSetId []byte, update pbuf.CardUpdate) error {
+	fullId := append(user.Id, cardSetId...)
+	key := manager.getKey(fullId)
+	history, ok := manager.userHistories[key]
+	if !ok {
+		return errors.New("Could not find history to update.")
+	}
+	for _, cardHistory := range history.History {
+		if reflect.DeepEqual(cardHistory.CardId, update.CardId) {
+			cardHistory.CurrentScore += update.Score
+			cardHistory.Scores = append(cardHistory.Scores, update.Score)
+			cardHistory.Indicies = append(cardHistory.Indicies, history.PlayIndex)
+			history.PlayIndex += 1
+		}
+	}
 	return nil
 }
 
-func (manager *InMemoryUserManagement) GetAuthInfoById(user pbuf.User, cardSetId []byte) pbuf.UserAuthInfo {
-	return pbuf.UserAuthInfo{}
+func (manager *InMemoryUserManagement) GetAuthInfoById(userId []byte) pbuf.UserAuthInfo {
+	key := manager.getKey(userId)
+	return manager.authInfo[key]
 }
 
 func (management *InMemoryUserManagement) AddUser(user pbuf.User, authInfo pbuf.UserAuthInfo) error {
