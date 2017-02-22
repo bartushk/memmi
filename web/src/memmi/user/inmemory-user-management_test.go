afd6fa9806d954303b288f910eb38385eb8ce8ca
Kyle Bartush <bartushk@gmail.com>
2017-02-21 21:48:42 -0800

    Quick test cleanup.

--- web/src/memmi/user/inmemory-user-management_test.go
+++ web/src/memmi/user/inmemory-user-management_test.go
@@ -21,25 +21,11 @@ func getMockedUserManagement() (*InMemoryUserManagement, *card.MockCardManagemen
 func Test_InMemoryUserManagement_NewInMemoryUserManagement(t *testing.T) {
 	newMan := NewInMemoryManagement()
 
-	if newMan.userIds == nil {
-		t.Error("userIds should be intitialized.")
-	}
-
-	if newMan.authInfo == nil {
-		t.Error("authInfo should be intitialized.")
-	}
-
-	if newMan.users == nil {
-		t.Error("users should be intitialized.")
-	}
-
-	if newMan.userHistories == nil {
-		t.Error("userHistories should be intitialized.")
-	}
-
-	if newMan.CardMan == nil {
-		t.Error("CardMan should be initialized.")
-	}
+	assert.NotNil(t, newMan.userIds)
+	assert.NotNil(t, newMan.authInfo)
+	assert.NotNil(t, newMan.users)
+	assert.NotNil(t, newMan.userHistories)
+	assert.NotNil(t, newMan.CardMan)
 }
 
 func Test_InMemoryUserManagement_GetHistory_CardSetError_ErrorReturned(t *testing.T) {
